52b6a6549283155f27b48a82b66960bd656aba63
Rishika Jain <66843549+rishikajain3012@users.noreply.github.com>
2026-08-12 17:50:16 +0000

    git

---
+++ day02_basics.py
@@ -0,0 +1,10 @@
+
+database = 'db2'
+
+tables = int(input("Enter number of tables: "))
+cpu = float(input("Enter CPU usage: "))
+
+print("DATABASE:", database)
+print("Tables:", tables)
+print("CPU Usage:", cpu)
+
